d151178448ac4724928af0d46e69c81c72654af3
Kim Cressman <kc2741@msstate.edu>
2025-04-02 14:31:56 -0500

    updated 'combine subbasins' file

--- R/Watershed Boundaries/combine MSEP with subbasins.R
+++ R/Watershed Boundaries/combine MSEP with subbasins.R
@@ -12,28 +12,10 @@ msep_0317_with_Subbasins <- st_read(here::here(in_path,
 msep_0318_with_Subbasins <- st_read(here::here(in_path,
                                                "0318_with_Subbasins.shp"), quiet = TRUE)
 
+msep_comb <- bind_rows(msep_0317_with_Subbasins, msep_0318_with_Subbasins)
 
-# Transform to UTM
-shapefile1_utm <- st_transform(msep_0317_with_Subbasins, 26916)
-shapefile2_utm <- st_transform(msep_0318_with_Subbasins, 26916)
 
-# Apply buffer approach with meter values
-buffer_distance <- 0.05 
-shapefile1_buffered <- st_buffer(shapefile1_utm, buffer_distance)
-shapefile2_buffered <- st_buffer(shapefile2_utm, buffer_distance)
-
-# Union the buffered geometries
-combined_utm <- st_union(shapefile1_buffered, shapefile2_buffered)
-plot(st_geometry(combined_utm))
-
-# Clean up the result with negative buffer
-combined_clean_utm <- st_buffer(combined_utm, -buffer_distance)
-
-# Transform back to original CRS if needed
-combined_clean <- st_transform(combined_clean_utm, 4269)
-plot(st_geometry(combined_clean))
-
-st_write(combined_clean, here::here("Data",
-                                    "Watershed Boundaries",
-                                    "combined",
-                                    "MSEP_withSubbasins.shp"))
+st_write(msep_comb, here::here("Data",
+                               "Watershed Boundaries",
+                               "combined",
+                               "MSEP_withSubbasins.shp"))
